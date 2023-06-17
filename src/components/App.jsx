10223061bd4ba3766a60672f763988d5644ea427
Szymon Oponczewski <szymon_o@mac.com>
2023-06-18 00:37:50 +0200

    app update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { nanoid } from "nanoid";
 import ContactForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
